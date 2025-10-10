b8d92552cc06f714e469d0d6fef7536ffd836fb3
rafly <raflyade9@gmail.com>
2025-10-10 16:18:45 +0700

    WIP

---
+++ internal/handler/handler_product_test.go
@@ -0,0 +1,13 @@
+package handler
+
+import(
+	// "errors"
+	// "go-ecommerce-cli/internal/entity"
+	// "github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestProductHandler_(t *testing.T) {
+
+}
