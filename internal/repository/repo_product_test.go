b57296518865d66046724e6e0e646978d5f00971
rafly <raflyade9@gmail.com>
2025-10-10 17:15:59 +0700

    WIP

--- internal/repository/repo_product_test.go
+++ internal/repository/repo_product_test.go
@@ -11,3 +11,13 @@ func TestRepoProduct_GetAll(t *testing.T) {
 func TestRepoProduct_AddProduct(t *testing.T) {
 	
 }
+
+func TestRepoProduct_DeleteProduct(t *testing.T) {
+	
+}
+func TestRepoProduct_UpdateProduct(t *testing.T) {
+
+}
+func TestRepoProduct_ShowProductById(t *testing.T) {
+
+}
